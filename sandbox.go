ea5720da672402ea82b942ab940717c6746b0d09
Sean Jezewski <seanwjezewski@gmail.com>
2016-03-08 17:52:09 -0800

    Update to use go templates. I was rewriting handlers when gin already does this.

--- sandbox.go
+++ sandbox.go
@@ -1,16 +1,20 @@
 package main
 
 import(
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/pachyderm/sandbox/src/handler"
 )
 
 var assetHandler = handler.NewAssetHandler()
-var pageHandler = handler.NewPageHandler()
+// var pageHandler = handler.NewPageHandler()
 
 func main() {
 	router := gin.Default()
+
+	router.LoadHTMLGlob("views/*")
 
 	assets := router.Group("/assets")
 	{
@@ -19,7 +23,10 @@ func main() {
 	}
 
 	router.GET("/", func (c *gin.Context) {
-		pageHandler.Serve("main", c)
+//		pageHandler.Serve("main", c)
+		c.HTML(http.StatusOK, "main.tmpl", gin.H{
+			"title" : "thing",
+		})
 	})
 
 	router.Run(":5678")
--- src/handler/handler.go
+++ src/handler/handler.go
@@ -6,7 +6,6 @@ import(
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/realistschuckle/gohaml"
 )
 
 type AssetHandler struct {
@@ -39,51 +38,7 @@ func (a *AssetHandler) Serve(c *gin.Context) {
 		a.files[path] = content
 	}
 
-	c.String(http.StatusOK, string(content) )
+	c.HTML(http.StatusOK, path, string(content) )
 }
 
 
-type PageHandler struct {
-	templates map[string][]byte
-}
-
-func NewPageHandler() *PageHandler {
-	return &PageHandler{
-		templates: make(map[string][]byte),
-	}
-}
-
-func (p *PageHandler) Serve(page string, c *gin.Context) {
-	path := fmt.Sprintf("views/%v.haml", page)
-
-	content, ok := p.templates[path]
-
-	if !ok || true { //Always read file while debugging
-		fmt.Println(path)
-		newContent, err := ioutil.ReadFile(path)
-
-		fmt.Printf("Found content: %v\n", string(newContent))
-
-		if err != nil {
-			fmt.Println(err)
-			c.String(http.StatusNotFound, "View not found")
-			return
-		}
-
-		p.templates[path] = newContent
-		content = newContent
-	}
-
-	fmt.Printf("Using tempalte: %v\n", content)
-
-	scope := make(map[string]interface{})
-	scope["context"] = c
-	engine, err := gohaml.NewEngine(string(content))
-	if err != nil {
-		fmt.Printf("err w engine: %v\n", err)
-	}
-
-	output := engine.Render(scope)
-
-	c.String(http.StatusOK, output)
-}
