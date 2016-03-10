26b0b4bd3de1925eba57ec9a5c0b48e2477d8cbb
Sean Jezewski <seanwjezewski@gmail.com>
2016-03-10 12:29:46 -0800

    Handoff gin router to http router and put main loop inside init(). Now the code is runnable by GAE (locally). I love it when its just a few config changes

--- app/app.go
+++ app/app.go
@@ -1,17 +1,54 @@
-package app
+package main
 
-import (
-        "fmt"
-        "net/http"
+import(
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/renders/multitemplate"
+
+	"github.com/pachyderm/sandbox/src/asset"
 )
 
-// init is called before the application starts.
+var assetHandler = asset.NewAssetHandler()
+var router = gin.New()
+
 func init() {
-        // Register a handler for /hello URLs.
-        http.HandleFunc("/", hello)
+
+	assets := router.Group("/assets")
+	{
+		assets.GET("/styles.css", assetHandler.Serve)
+		assets.GET("/main.js", assetHandler.Serve)
+	}
+
+	router.HTMLRender = loadTemplates()
+
+	router.GET("/", handle("main"))
+
+	http.Handle("/", router)
 }
 
-// hello is an HTTP handler that prints "Hello Gopher!"
-func hello(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Hello, Gopher!")
+func handle(page string) ( func (*gin.Context) ){
+	return func(c *gin.Context) {
+		if gin.Mode() == "debug" {
+			router.HTMLRender = loadTemplates()
+		}
+		
+		c.HTML(http.StatusOK, page, gin.H{
+			"title" : "thing",
+		})
+	}
+}
+
+func loadTemplates() multitemplate.Render {
+	templates := multitemplate.New()
+	templates.AddFromFiles(
+		"main",
+		"views/base.html",
+		"views/main.html",
+		"views/data.html",
+		"views/copy.html",
+		"views/code.html",
+	)
+
+	return templates
 }
