7b03496cf7848f2356f588b2c71993314c9fc3fe
Sean Jezewski <seanwjezewski@gmail.com>
2016-03-11 10:00:13 -0800

    Try vendorizing local files too

---
+++ vendor/github.com/pachyderm/sandbox/app/app.go
@@ -0,0 +1,54 @@
+package app
+
+import(
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/renders/multitemplate"
+
+	"github.com/pachyderm/sandbox/src/asset"
+)
+
+var assetHandler = asset.NewAssetHandler()
+var router = gin.New()
+
+func init() {
+
+	assets := router.Group("/assets")
+	{
+		assets.GET("/styles.css", assetHandler.Serve)
+		assets.GET("/main.js", assetHandler.Serve)
+	}
+
+	router.HTMLRender = loadTemplates()
+
+	router.GET("/", handle("main"))
+
+	http.Handle("/", router)
+}
+
+func handle(page string) ( func (*gin.Context) ){
+	return func(c *gin.Context) {
+		if gin.Mode() == "debug" {
+			router.HTMLRender = loadTemplates()
+		}
+		
+		c.HTML(http.StatusOK, page, gin.H{
+			"title" : "thing",
+		})
+	}
+}
+
+func loadTemplates() multitemplate.Render {
+	templates := multitemplate.New()
+	templates.AddFromFiles(
+		"main",
+		"views/base.html",
+		"views/main.html",
+		"views/data.html",
+		"views/copy.html",
+		"views/code.html",
+	)
+
+	return templates
+}
