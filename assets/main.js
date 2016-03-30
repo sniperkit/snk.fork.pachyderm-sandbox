8a266a515c70e0b73d1ec33d62f6a6cf5476e1f2
Sean Jezewski <seanwjezewski@gmail.com>
2016-03-29 17:56:32 -0700

    Start filling in UI that polls to see if a job has completed.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,32 @@
-console.log("hai");
+// Poller to check if pipeline is done
+
+function updatePipelineStatusUI(result) {
+    console.log(result);
+    console.log(result["status"]);
+
+    $(".pane.code").removeClass("disabled");
+
+    for(var outputRepo in result["states"]) {
+        var statusBar = $(".status .item.hidden").clone().removeClass("hidden");
+        
+        for(var commitID in result["states"][outputRepo]) {
+            var state = result["states"][outputRepo][commitID];
+            statusBar.text( outputRepo + " completed commit (" + commitID + ")" + state );
+            statusBar.appendTo(".status");
+        }
+    }
+
+    window.clearInterval(pipelineStatusPoller);
+}
+
+function checkPipelineStatus() {
+
+    $(".pane.code").addClass("disabled");
+
+    $.ajax({
+            url: "/check_pipeline_status",
+            success: updatePipelineStatusUI
+    });
+}
+
+var pipelineStatusPoller = window.setInterval(checkPipelineStatus, 250);
